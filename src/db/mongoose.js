6fa1755c5311817a033278249939b440b8862bf1
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2019-06-24 13:02:17 -0500

    Correction

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,57 +2,10 @@ const mongoose = require('mongoose')
 
 
 
-mongoose.connect("mongodb+srv://dbPrueba:Pammap99*@cluster0-viegr.mongodb.net/prototipo-efn?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 }).catch((e)=>{
     console.log(e)
 })
-
-// const Factura = mongoose.model('facturasPublicadas', {
-//     rfc: {
-//         type: String,
-//         validate(value){
-//             if(value<0){
-//                 throw new Error('RFC must be a positive number.')
-//             }
-//         },
-//     },
-//     numero:{
-//         type: Number
-//     }
-// //     folioFiscal:{
-// //         type: String
-// //     },
-// //     invoiceDate:{
-// //         type: Date
-// //     },
-// //     DueDate:{
-// //         type:Date
-// //     },
-// //     moneda:{
-// //         type: String
-// //     },
-// //     aforo:
-// //    { type:Number}
-// })
-
-// // Factura.get('/facturas', async (req, res)=>{
-// //     try{
-// //         const facturas = await Factura.find({})
-// //         res.send(facturas)
-// //     }
-// //     catch(e){
-// //         res.status(500).send(e.message)
-// //     }
-// // })
-
-
-// new Factura({
-//     "rfc":"384b43vvv",
-//     "number":329874
-// }).save().then((result)=>{
-//     console.log(result)
-// }).catch((error)=>{console.log(error)})
-
