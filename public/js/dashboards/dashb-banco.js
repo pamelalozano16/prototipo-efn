9b01454858b92f9a0be36e7393f566cce4654981
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2019-08-21 16:10:00 -0500

    ignore x frames extension

--- public/js/dashboards/dashb-banco.js
+++ public/js/dashboards/dashb-banco.js
@@ -1,1 +1,24 @@
 console.log("hola")
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://170.128.120.91:8080/ibi_apps/rs/ibfs?IBIRS_action=signOn&IBIRS_userName=efactor1&IBIRS_password=efactor1",
+    "method": "GET",
+    "headers": {
+        "Access-Control-Allow-Origin":"true",
+      "Accept": "*/*",
+      "Cache-Control": "no-cache",
+    "Host": "170.128.120.91:8080",
+      "Accept-Encoding": "gzip, deflate",
+      "Connection": "keep-alive",
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+var url = "http://170.128.120.91:8080/ibi_apps/WFServlet?IBIF_ex=efactor_t2_g1_e.fex";
+document.getElementById("theFrame").src=url;
